048c54dd93607c6d45def00da5a4b36de2438bbb
Pipatchies <ariane.bertaud@gmail.com>
2025-09-17 13:51:29 +0200

    feat(api): create test endpoint

--- api/src/core/views.py
+++ api/src/core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+def test_api(request):
+    return JsonResponse({
+        "status": "ok",
+        "message": "Test endpoint is working!"
+    })
+
