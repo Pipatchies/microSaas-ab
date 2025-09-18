4dc5897850d08164283c93eb008834f24e687f00
Pipatchies <ariane.bertaud@gmail.com>
2025-09-18 09:57:03 +0200

    feat(api): add health-check endpoint to test database connection

--- api/src/core/views.py
+++ api/src/core/views.py
@@ -1,4 +1,5 @@
 from django.http import JsonResponse
+from django.db import connection
 
 def test_api(request):
     return JsonResponse({
@@ -6,3 +7,19 @@ def test_api(request):
         "message": "Test endpoint is working!"
     })
 
+def test_db(request):
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute("SELECT version();")  # Récupère la version PostgreSQL
+            version = cursor.fetchone()[0]
+
+        return JsonResponse({
+            "status": "ok",
+            "message": f"API connected to database!",
+            "db_version": version
+        })
+    except Exception as e:
+        return JsonResponse({
+            "status": "error",
+            "message": str(e)
+        }, status=500)
