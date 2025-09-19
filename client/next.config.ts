6b8312675490be171001e371ed3607288ee225bc
Pipatchies <ariane.bertaud@gmail.com>
2025-09-19 16:24:25 +0200

    chore(docker): configure hot reload for Next.js in Docker

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpackDevMiddleware: (config: any) => {
+    config.watchOptions = {
+      poll: 1000, // vérifie toutes les secondes
+      aggregateTimeout: 300,
+    }
+    return config
+  },
 };
 
 export default nextConfig;
