193594a4de59da84df1cf5c4256b1b726f5229dc
Pipatchies <ariane.bertaud@gmail.com>
2025-10-13 10:06:25 +0200

    chore(lint): clean codebase and fix eslint/prettier issues across frontend

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,12 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   webpackDevMiddleware: (config: any) => {
     config.watchOptions = {
       poll: 1000, // vérifie toutes les secondes
       aggregateTimeout: 300,
-    }
-    return config
+    };
+    return config;
   },
 };
 
