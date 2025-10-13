aaec1990dd0bbea79ca7c53f3ab5ca5a004ee3d7
Pipatchies <ariane.bertaud@gmail.com>
2025-10-13 08:36:01 +0200

    chore(api): load environment variables from .env using django-environ

--- api/src/config/settings.py
+++ api/src/config/settings.py
@@ -1,9 +1,14 @@
 from pathlib import Path
+import environ
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = environ.Env()
+
+env_file = BASE_DIR / ".env"
+environ.Env.read_env(env_file)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
