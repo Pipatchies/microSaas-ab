20d6ac3055cbb9e6ac8f6569f771f1c4eed8a6cd
Pipatchies <ariane.bertaud@gmail.com>
2025-11-13 15:25:29 +0100

    fix(nextjs): ensure hot reload works both locally and in Docker

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  webpackDevMiddleware: (config: any) => {
-    config.watchOptions = {
-      poll: 1000, // vérifie toutes les secondes
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
