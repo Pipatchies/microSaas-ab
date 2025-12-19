8986c5a0755ea114e623445e9693c0fe657f68f3
Pipatchies <ariane.bertaud@gmail.com>
2025-12-19 14:34:28 +0100

    chore: maj nextjs in client

--- client/eslint.config.mjs
+++ client/eslint.config.mjs
@@ -1,26 +1,44 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginNext from "@next/eslint-plugin-next";
 import pluginPrettier from "eslint-plugin-prettier";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  js.configs.recommended,
   {
     plugins: {
+      react: pluginReact,
+      "react-hooks": pluginReactHooks,
+      "@next/next": pluginNext,
       prettier: pluginPrettier,
     },
     rules: {
+      // Replicating recommended rules manually since plugins not yet fully flat-ready
+      ...pluginReact.configs.recommended.rules,
+      ...pluginReactHooks.configs.recommended.rules,
+      ...pluginNext.configs.recommended.rules,
+      ...pluginNext.configs["core-web-vitals"].rules,
+
       "prettier/prettier": "warn",
+
+      // React 17+ JSX Transform
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
     },
   },
-
   {
     ignores: [
       "node_modules/**",
@@ -31,5 +49,3 @@ const eslintConfig = [
     ],
   },
 ];
-
-export default eslintConfig;
