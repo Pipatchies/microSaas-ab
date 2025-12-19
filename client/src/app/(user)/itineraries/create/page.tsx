84f3201e73a13275f7510e6ccb18ecda8c2be3bb
Pipatchies <ariane.bertaud@gmail.com>
2025-12-19 19:00:15 +0100

    feat: create itineraryForm and initialize it in create page

---
+++ client/src/app/(user)/itineraries/create/page.tsx
@@ -0,0 +1,9 @@
+import ItineraryForm from "../components/ItineraryForm";
+
+export default function CreateItineraryPage() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <ItineraryForm />
+    </div>
+  );
+}
