3ec1dd2d84245a48a53c24bd844c41a81e835e53
Pipatchies <ariane.bertaud@gmail.com>
2026-03-01 17:10:39 +0100

    chore(config): load rubik light font

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -12,7 +12,7 @@ const nanumMyeongjo = Nanum_Myeongjo({
 });
 
 const rubik = Rubik({
-  weight: ["400", "500", "600", "700"],
+  weight: ["300", "400", "500", "600", "700"],
   subsets: ["latin"],
   variable: "--font-subtitle",
 });
