b7c5be82bcb95432ff9d0c4eb25692c55d444734
Pipatchies <ariane.bertaud@gmail.com>
2026-03-01 21:27:01 +0100

    feat(client): add new itinerary button to itineraries page

--- client/src/app/(user)/itineraries/page.tsx
+++ client/src/app/(user)/itineraries/page.tsx
@@ -2,16 +2,27 @@
 
 import Typography from "@/components/typography";
 import { ItineraryList } from "../../../components/itinerary-list";
+import { CtaButton } from "@/components/cta-button";
+import Link from "next/link";
 
 export default function ItinerariesPage() {
   return (
-    <div className="container mx-auto py-8 px-4 md:px-0">
-      <div className="mb-4">
+    <div className="container mx-auto pt-8 pb-32 px-4 md:px-0">
+      <section className="mb-4">
         <Typography variant="h2" className="text-3xl font-bold tracking-tight">
           A LA UNE
         </Typography>
+      </section>
+      <section>
+        <ItineraryList />
+      </section>
+
+      {/* Floating Action Button */}
+      <div className="fixed bottom-24 left-1/2 -translate-x-1/2 z-40 w-max flex justify-center">
+        <Link href="/itineraries/create">
+          <CtaButton text="Ajouter un itinéraire" ctaVariant="solid" />
+        </Link>
       </div>
-      <ItineraryList />
     </div>
   );
 }
