6a0f75e1b3d7b2b641921d608cf4f135340fecf5
Pipatchies <ariane.bertaud@gmail.com>
2026-03-01 17:10:21 +0100

    chore(config): add unsplash domain

--- client/next.config.ts
+++ client/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
   webpack: (config) => {
     config.watchOptions = {
       poll: 1000,
