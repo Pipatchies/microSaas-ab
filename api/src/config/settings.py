48e90ed08ec3e1b7ce794bdd8bd3a9de6d03a80e
Pipatchies <ariane.bertaud@gmail.com>
2026-03-02 08:15:30 +0100

    feat(api): add foodplace app with models

--- api/src/config/settings.py
+++ api/src/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "core",
     "itineraries",
+    "places",
     "corsheaders",
     "rest_framework",
 ]
---
+++ api/src/places/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+class FoodPlace(models.Model):
+    id_foodplace = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    type = models.CharField(max_length=100)
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    description = models.TextField(blank=True)
+    mapbox_id = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
