6580f6973a60d3ecb54f026906641ef7bab2f715
Pipatchies <ariane.bertaud@gmail.com>
2026-03-02 09:33:58 +0100

    feat(integration): connect frontend to DRF backend via fetch and cors

--- api/src/config/settings.py
+++ api/src/config/settings.py
@@ -137,6 +137,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost:3300",
+    "http://127.0.0.1:3300",
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
