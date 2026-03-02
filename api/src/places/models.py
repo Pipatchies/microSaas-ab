ce0e4108258ad0649edafa14c20cd1325c2fae37
Pipatchies <ariane.bertaud@gmail.com>
2026-03-02 11:13:09 +0100

    fix(api): make type field optional on FoodPlace model

--- api/src/places/models.py
+++ api/src/places/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class FoodPlace(models.Model):
     id_foodplace = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200)
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, blank=True, null=True)
     longitude = models.FloatField()
     latitude = models.FloatField()
     description = models.TextField(blank=True)
