6b671cc1076a556c8e762404af7ba52231d686d6
Pipatchies <ariane.bertaud@gmail.com>
2026-03-02 09:48:00 +0100

    feat(integration): connect frontend services for steps and food places to drf backend

--- api/src/itineraries/urls.py
+++ api/src/itineraries/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ItineraryViewSet
+from .views import ItineraryViewSet, StepViewSet
 
 router = DefaultRouter()
 router.register(r"itineraries", ItineraryViewSet)
+router.register(r"steps", StepViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
--- api/src/itineraries/views.py
+++ api/src/itineraries/views.py
@@ -1,8 +1,13 @@
 from rest_framework import viewsets
-from .models import Itinerary
-from .serializers import ItinerarySerializer
+from .models import Itinerary, Step
+from .serializers import ItinerarySerializer, StepSerializer
 
 
 class ItineraryViewSet(viewsets.ModelViewSet):
     queryset = Itinerary.objects.all()
     serializer_class = ItinerarySerializer
+
+
+class StepViewSet(viewsets.ModelViewSet):
+    queryset = Step.objects.all()
+    serializer_class = StepSerializer
